5da91c2ca523e7e43ee030e16d209cbfd8f2ab32
vsilent <jabberroid@gmail.com>
2019-09-23 10:23:01 +0300

    split up opencart container into php and nginx separate containers

--- tests.py
+++ tests.py
@@ -18,13 +18,17 @@ for c in client.containers.list():
         print(c.logs())
 
 # # NGINX
-nginx = client.containers.get('opencart')
+php = client.containers.get('opencart')
+php_out = php.exec_run("php-fpm7.1 -t").output.decode()
+
+nginx = client.containers.get('opencart-nginx')
 nginx_cfg = nginx.exec_run("/usr/sbin/nginx -T")
+
 assert nginx.status == 'running'
 print(nginx_cfg.output.decode())
 print(nginx.logs())
-assert "php-fpm entered RUNNING state" in nginx.logs()
-assert "success: cron entered RUNNING state" in nginx.logs()
+assert "php-fpm entered RUNNING state" in php.logs()
+assert "success: cron entered RUNNING state" in php.logs()
 assert "nginx entered RUNNING state" in nginx.logs()
 assert 'server_name _;' in nginx_cfg.output.decode()
 assert "error_log /proc/self/fd/2" in nginx_cfg.output.decode()
@@ -33,7 +37,7 @@ assert 'configuration file /etc/nginx/nginx.conf test is successful' in nginx_cf
 assert 'HTTP/1.1" 500' not in nginx.logs()
 
 
-db = client.containers.get('db')
+db = client.containers.get('opencart-db')
 assert db.status == 'running'
 cnf = db.exec_run("/usr/sbin/mysqld --verbose  --help")
 db_log = db.logs()
@@ -42,8 +46,6 @@ assert "mysqld: ready for connections" in db_log.decode()
 assert "Version: '5.7" in db_log.decode()
 
 # check redirect to web installer
-app = client.containers.get('opencart')
-
 response = requests.get("http://localhost")
 assert '<base href="http://localhost/install/" />' in response.text
 assert 'License agreement' in response.text
